eaa7d549042489db9117b27c2961574bc72ba755
jack6785123 <32056027+jack6785123@users.noreply.github.com>
2018-03-20 12:16:31 +0800

    Add files via upload

--- home/src/Product.java
+++ home/src/Product.java
@@ -1,8 +1,15 @@
 public  class Product{
-
-     public Product (String name1, float price1, int g1){
-
+    private String name;
+    private float price;
+    private int num = 1;
+     public Product (String name1, float price1, int num1){
+         this.name = name1;
+         this.price = price1;
+         this.num = num1;
      }
+     public void showInfo{
+
+    }
 }
 
 
